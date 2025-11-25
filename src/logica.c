69a5794a916a083d48e7e12a13927d4327467fc6
Santos-dev25 <caua34584@gmail.com>
2025-11-25 09:20:04 -0300

    corrigindo logica.c

--- src/logica.c
+++ src/logica.c
@@ -1,8 +1,7 @@
-#include <stdlib.h> // Necessário para abs()
+#include <stdlib.h> // abs
 #include "../include/logica.h"
 
 int posicao_valida(int x, int y, int maxLinhas, int maxColunas) {
-    // Verifica se x e y estão dentro das dimensões da matriz
     if (x >= 0 && x < maxColunas && y >= 0 && y < maxLinhas) {
         return 1;
     }
@@ -10,6 +9,5 @@ int posicao_valida(int x, int y, int maxLinhas, int maxColunas) {
 }
 
 int calcular_distancia(int x1, int y1, int x2, int y2) {
-    // Distância de Manhattan (passos absolutos na grade)
     return abs(x1 - x2) + abs(y1 - y2);
 }
